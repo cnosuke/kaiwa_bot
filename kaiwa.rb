443cff8452246a4d4312886fd99c31e811ee6cbc
Shinnosuke at MELCHIOR <shinnosuke@gmail.com>
2012-10-16 15:02:13 +0900

    きれいにした

--- kaiwa.rb
+++ kaiwa.rb
@@ -3,13 +3,5 @@ while line = STDIN.gets
   if (/exit/ =~ line)
     break;
   end
-  n = rand(3).to_i
-  if n==0
-    puts 'マジで'
-  elsif n==1
-    puts 'ヤバイ'
-  else
-    puts 'ウケるー'
-  end
-  
+  puts ['マジで', 'ヤバイ', 'ウケるー'].sample
 end
